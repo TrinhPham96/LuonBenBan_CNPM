96091e8663303bdc9e8cd500597d827549fbdac6
minhminh <minhm9986@gmail.com>
2017-06-18 02:15:23 +0700

    update2
    cắt temp

---
+++ local/resources/views/module/cont2.blade.php
@@ -0,0 +1,69 @@
+<div class="grid_2" id="tender">
+	<div class="container">
+		<div class="sub-hd">
+			<h3 class="tittle">TIN TỨC <span>Y TẾ</span></h3>
+		</div>
+		<div class="box_1 one">
+			<div class="col-md-6">
+				<div class="col_1_of_2 span_1_of_2 multi-gd-text">
+					<a href="single.html"><img src="images/n1.jpg" class="img-responsive" alt=""></a>
+					<div class="story_desc">
+						<h3>Mark McRoberts</h3>
+						<p>New York</p>
+					</div>
+				</div>
+				<div class="col_1_of_2 span_1_of_2">
+					<h4>Volunteer Firefighter Threatened By Tongue Tumor</h4>
+					<p class="p1">Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim venia.</p>
+				</div>
+				<div class="clearfix"></div>
+			</div>
+			<div class="col-md-6">
+				<div class="col_1_of_2 span_1_of_2 multi-gd-text">
+					<a href="single.html"><img src="images/n5.jpg" class="img-responsive" alt=""></a>
+					<div class="story_desc">
+						<h3>Mark McRoberts</h3>
+						<p>New York</p>
+					</div>
+				</div>
+				<div class="col_1_of_2 span_1_of_2">
+					<h4>Volunteer Firefighter Threatened By Tongue Tumor</h4>
+					<p class="p1">Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim venia.</p>
+				</div>
+				<div class="clearfix"></div>
+			</div>
+			<div class="clearfix"></div>
+		</div>
+		<div class="box_2">
+			<div class="col-md-6">
+				<div class="col_1_of_2 span_1_of_2 multi-gd-text">
+					<a href="single.html"><img src="images/n3.jpg" class="img-responsive" alt=""></a>
+					<div class="story_desc">
+						<h3>Mark McRoberts</h3>
+						<p>New York</p>
+					</div>
+				</div>
+				<div class="col_1_of_2 span_1_of_2">
+					<h4>Volunteer Firefighter Threatened By Tongue Tumor</h4>
+					<p class="p1">Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim venia.</p>
+				</div>
+				<div class="clearfix"></div>
+			</div>
+			<div class="col-md-6">
+				<div class="col_1_of_2 span_1_of_2 multi-gd-text">
+					<a href="single.html"><img src="images/n4.jpg" class="img-responsive" alt=""></a>
+					<div class="story_desc">
+						<h3>Mark McRoberts</h3>
+						<p>New York</p>
+					</div>
+				</div>
+				<div class="col_1_of_2 span_1_of_2">
+					<h4>Volunteer Firefighter Threatened By Tongue Tumor</h4>
+					<p class="p1">Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim venia.</p>
+				</div>
+				<div class="clearfix"></div>
+			</div>
+			<div class="clearfix"></div>
+		</div>
+	</div>
+</div>
--- local/resources/views/widgets/header.blade.php
+++ local/resources/views/widgets/header.blade.php
@@ -1,7 +1,6 @@
-<!--header-top-->
-			<div class="header-top">
+
 			  <div class="container">
-					 <nav class="navbar navbar-default">
+			  	<nav class="navbar navbar-default">
 					<!-- Brand and toggle get grouped for better mobile display -->
 					<div class="navbar-header">
 					  <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
@@ -11,20 +10,48 @@
 						<span class="icon-bar"></span>
 					  </button>
 						<div class="logo">
-							<h1><a class="navbar-brand" href="index.html"><span>M</span>edicinal  <img src="images/logo.png" alt=" " /></a></h1>
+							<h1><a class="navbar-brand" href="index.html"><span>LUÔN</span> BÊN BẠN  <img src="images/logo.png" alt=" " /></a></h1>
+						</div>
+						
+					</div>
+					<div class="imgLogin">
+						<a href="javascript:;" class="user-profile dropdown-toggle" data-toggle="dropdown" aria-expanded="false">
+							<img src="images/ab.jpg" alt="">Chinh 18+
+							<i class="fa fa-sort-desc" aria-hidden="true"></i>
+						</a>
+						<ul class="dropdown-menu">
+							<li><a href="javascript:;"><i class="icon-edit"></i> Thông tin cá nhân</a></li>
+							<li><a href="login.html"><i class="fa fa-sign-out"></i> Đăng xuất</a></li>
+						</ul>
+					</div>
+					<div class="collapse navbar-collapse nav-wil" id="bs-example-navbar-collapse-1">
+						<div class="top-menu">
+							<nav class="menu menu--francisco">
+								<ul class="nav navbar-nav menu__list">
+									<li class="menu__item menu__item--current"><a href="index.html" class="menu__link"><span class="menu__helper">Trang chủ</span></a></li>
+									<li class="menu__item"><a href="about.html" class="menu__link"><span class="menu__helper">Tra cứu</span></a></li>
+									<li class="menu__item"><a href="typography.html" class="menu__link"><span class="menu__helper">Trắc nghiệm</span></a></li>
+									<li class="menu__item"><a href="gallery.html" class="menu__link"><span class="menu__helper">Hỗ trợ</span></a></li>
+									<li class="menu__item"><a href="contact.html" class="menu__link"><span class="menu__helper">Thông tin</span></a></li>
+									<li class ="menu__item"> 
+										<div id="custom-search-input">
+
+											<div class="input-group ">
+												<input type="text" class="  search-query form-control" placeholder="Nhập từ khóa tìm kiếm..." />
+												<span class="input-group-btn">
+													<button class="btn btn-danger" type="button">
+														<span class=" glyphicon glyphicon-search"></span>
+													</button>
+												</span>
+											</div>
+
+										</div></li>
+								</ul>
+							</nav>
 						</div>
 					</div>
 
 					<!-- Collect the nav links, forms, and other content for toggling -->
-					<div class="collapse navbar-collapse nav-wil" id="bs-example-navbar-collapse-1">
-						  <div class="top-menu">
-							<nav class="menu menu--francisco">
-									<ul class="nav navbar-nav menu__list">
-										<li class="menu__item menu__item--current"><a href="index.html" class="menu__link"><span class="menu__helper">Home</span></a></li>
-										<li class="menu__item"><a href="about.html" class="menu__link"><span class="menu__helper">About Us</span></a></li>
-										<li class="menu__item"><a href="gallery.html" class="menu__link"><span class="menu__helper">Departments</span></a></li>
-										<li class="menu__item"><a href="contact.html" class="menu__link"><span class="menu__helper">Contact Us</span></a></li>
-									</ul>
-								</nav>
-							</div>
-					</div>
+					
+				</nav>
+			</div>
