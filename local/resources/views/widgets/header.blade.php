e38f4fdc6621bf0f8e09a112db4777e7e26f0f22
minhminh <minhm9986@gmail.com>
2017-06-18 00:55:19 +0700

    update 1
    cat temp

---
+++ local/resources/views/widgets/header.blade.php
@@ -0,0 +1,30 @@
+<!--header-top-->
+			<div class="header-top">
+			  <div class="container">
+					 <nav class="navbar navbar-default">
+					<!-- Brand and toggle get grouped for better mobile display -->
+					<div class="navbar-header">
+					  <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+						<span class="sr-only">Toggle navigation</span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+					  </button>
+						<div class="logo">
+							<h1><a class="navbar-brand" href="index.html"><span>M</span>edicinal  <img src="images/logo.png" alt=" " /></a></h1>
+						</div>
+					</div>
+
+					<!-- Collect the nav links, forms, and other content for toggling -->
+					<div class="collapse navbar-collapse nav-wil" id="bs-example-navbar-collapse-1">
+						  <div class="top-menu">
+							<nav class="menu menu--francisco">
+									<ul class="nav navbar-nav menu__list">
+										<li class="menu__item menu__item--current"><a href="index.html" class="menu__link"><span class="menu__helper">Home</span></a></li>
+										<li class="menu__item"><a href="about.html" class="menu__link"><span class="menu__helper">About Us</span></a></li>
+										<li class="menu__item"><a href="gallery.html" class="menu__link"><span class="menu__helper">Departments</span></a></li>
+										<li class="menu__item"><a href="contact.html" class="menu__link"><span class="menu__helper">Contact Us</span></a></li>
+									</ul>
+								</nav>
+							</div>
+					</div>
