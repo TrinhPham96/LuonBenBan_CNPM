a7b3b02799af4fe222debfc9c93b60bda469483e
TrinhPham96 <nhoccodaihp96@gmail.com>
2017-06-20 00:00:07 +0700

    Thêm ds khoa (update)

---
+++ local/resources/views/module/specialList_cont.blade.php
@@ -0,0 +1,84 @@
+<div class="main">
+
+	<ol class="breadcrumb">
+		<li><a href="#">Trang chủ</a></li>
+		<li><a href="/LuonBenBan_CNPM/tra-cuu">Tra cứu</a></li>
+		<li class="active">Danh sách khoa</li>
+	</ol>
+
+
+	<div class="medicinal-w3">
+		<div class="container">
+			<div class="sub-hd">
+				<h3 class="tittle two">Các chuyên khoa</h3>
+			</div>
+			<div class="medicinal-w3-grids one">
+				<div class="col-md-6 medicinal-w3-grid">
+					<div class="col-md-8 medicinal-w3-left">
+						<h4>1.Sản phụ khoa</h4>
+						<p>Là một bốn phân ngành y khoa quan trọng của khối lâm sàng. Khoa có chức năng chẩn đoán và điều trị các bệnh lý và dự phòng sức khỏe của nữ giới, bao gồm phụ nữa còn độc thân và đã mang thai, sinh con. Khoa có thể được phân tách thành Sản khoa và phụ khoa.</p>
+					</div>
+					<div class="col-md-4 medicinal-w3-right">
+						<div class="hi-icon-wrap hi-icon-effect-7 hi-icon-effect-7b">
+							<a href="#" class="hi-icon icon2"></a>
+						</div>
+					</div>
+					<div class="clearfix"></div>
+				</div>
+				<div class="col-md-6 medicinal-w3-grid">
+					<div class="col-md-4 medicinal-w3-right">
+						<div class="hi-icon-wrap hi-icon-effect-7 hi-icon-effect-7b">
+							<a href="#" class="hi-icon icon2"></a>
+						</div>
+					</div>
+					<div class="col-md-8 medicinal-w3-left1">
+						<h4>2. Da liễu</h4>
+						<p>Là một bốn phân ngành y khoa quan trọng của khối lâm sàng. Khoa có chức năng chẩn đoán và điều trị các bệnh lý và dự phòng sức khỏe của nữ giới, bao gồm phụ nữa còn độc thân và đã mang thai, sinh con. Khoa có thể được phân tách thành Sản khoa và phụ khoa.</p>
+					</div>
+					<div class="clearfix"></div>
+				</div>
+				<div class="clearfix"></div>
+			</div>
+			<div class="medicinal-w3-grids">
+				<div class="col-md-6 medicinal-w3-grid">
+					<div class="col-md-8 medicinal-w3-left">
+						<h4>3. Nhi khoa</h4>
+						<p>Là một bốn phân ngành y khoa quan trọng của khối lâm sàng. Khoa có chức năng chẩn đoán và điều trị các bệnh lý và dự phòng sức khỏe của nữ giới, bao gồm phụ nữa còn độc thân và đã mang thai, sinh con. Khoa có thể được phân tách thành Sản khoa và phụ khoa.</p>
+					</div>
+					<div class="col-md-4 medicinal-w3-right">
+						<div class="hi-icon-wrap hi-icon-effect-7 hi-icon-effect-7b">
+							<a href="#" class="hi-icon icon2"></a>
+						</div>
+					</div>
+					<div class="clearfix"></div>
+				</div>
+				<div class="col-md-6 medicinal-w3-grid">
+					<div class="col-md-4 medicinal-w3-right">
+						<div class="hi-icon-wrap hi-icon-effect-7 hi-icon-effect-7b">
+							<a href="#" class="hi-icon icon2"></a>
+						</div>
+					</div>
+					<div class="col-md-8 medicinal-w3-left1">
+						<h4>4. Khoa chỉnh hình</h4>
+						<p>Là một bốn phân ngành y khoa quan trọng của khối lâm sàng. Khoa có chức năng chẩn đoán và điều trị các bệnh lý và dự phòng sức khỏe của nữ giới, bao gồm phụ nữa còn độc thân và đã mang thai, sinh con. Khoa có thể được phân tách thành Sản khoa và phụ khoa.</p>
+					</div>
+					<div class="clearfix"></div>
+				</div>
+				<div class="clearfix"></div>
+			</div>
+		</div>
+	</div>
+
+	<nav>
+		<ul class="pagination">
+			<li class="disabled"><a href="#" aria-label="Previous"><span aria-hidden="true">«</span></a></li>
+			<li class="active"><a href="#">1 <span class="sr-only">(current)</span></a></li>
+			<li><a href="#">2</a></li>
+			<li><a href="#">3</a></li>
+			<li><a href="#">4</a></li>
+			<li><a href="#">5</a></li>
+			<li><a href="#" aria-label="Next"><span aria-hidden="true">»</span></a></li>
+		</ul>
+	</nav>
+
+</div>
